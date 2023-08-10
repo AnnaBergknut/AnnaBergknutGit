9fc5febc147da28498435987c19f3e3c1bafb675
AnnaBergknut <annanejla@gmail.com>
2023-08-10 14:21:32 +0200

    DatastrukA

--- BTH/datastruk/UpgiftA/test_fixtur/sorting.py
+++ BTH/datastruk/UpgiftA/test_fixtur/sorting.py
@@ -4,8 +4,9 @@ Written by Anna Bergknut, October 2022
 
 import sys
 import time
-from pathlib import Path
+import math
 import numpy as np
+
 
 version = sys.version_info
 if version[0] < 3 :
@@ -15,8 +16,8 @@ elif version[1] < 6:
     print(f'Please install python 3, at least version 3.6. You have {version[0]}.{version[1]}.')
     sys.exit(1)
 
-def quicksort(lst: list) -> None: 
-    """ Quicksort uses a divide and conquer method by dividing the list in to two lists based if it is bigger or smaller the the pivot. With an average speed of O(nlog {n})  """
+def quicksort(lst: list) -> None:
+    """ Quicksort divide the list in to two lists based if it is bigger or smaller then pivot. O(nlog(n)) """
     low = 0
     high = len(lst)-1
     my_quicksort(lst, low, high)
@@ -40,7 +41,7 @@ def partition(lst, low, high):
     return j
 
 def insertionsort(lst:list) -> None:
-    """ Insertionsort assume that the first element is sorted and then insert the next elements on a sorted spot. With an average speed of O( n^2) """
+    """ Insertionsort the first element is sorted then insert the next elements. O( n^2) """
     low = 0
     high = len(lst)-1
     my_insertionsort(lst, low, high)
@@ -52,14 +53,14 @@ def my_insertionsort(lst, low, high):
         while j>low and lst[j-1]>number:
             lst[j]= lst[j-1]
             j-= 1
-        lst[j]= number       
+        lst[j]= number
     return lst
 
 def mergesort(lst:list) -> None:
-    """ Mergesort is a divide and conquer algorithm that divide until we have only lonely elements and then merge dem sorted.. With an average speed of O(nlog {n}) """
+    """ Mergesort divide until only lonely elements and then merge. O(nlog(n)) """
     temp = my_mergesort(lst)
     for i in range(0, len(temp)):
-        lst[i] = temp[i]      
+        lst[i] = temp[i]
 
 def my_mergesort(lst:list):
     rightindex = len(lst)
@@ -71,7 +72,7 @@ def my_mergesort(lst:list):
         left = my_mergesort(lst[:middleindex])
         right = my_mergesort(lst[middleindex:])
         merged = merge(left, right)
-        return merged       
+        return merged
 
 def merge(left, right):
     result = []
@@ -92,7 +93,7 @@ def merge(left, right):
     return result
 
 def mergesort_hybrid(lst:list) -> None:
-    """ Mergesort and insertionsort hybrid. Ones mergesort have divided the data in to smaller lists of less then 10 the insertion sort will sort it because that is a lot faster. """
+    """ Mergesort and insertionsort hybrid. """
     temp = my_mergesort_hybrid(lst)
     for i in range(0, len(temp)):
         lst[i] = temp[i]
@@ -100,34 +101,31 @@ def mergesort_hybrid(lst:list) -> None:
 def my_mergesort_hybrid(lst:list):
     low = 0
     high = len(lst)
-    if high-low + 1 < 10:
+    if high-low + 1 < 86:
         high = high -1
         temp = my_insertionsort(lst, low, high)
         high = high +1
         return temp
     elif low < high:
-        mid = ((low+high)//2)
+        mid = (low+high)//2
         left = my_mergesort_hybrid(lst[low:mid])
         right = my_mergesort_hybrid(lst[mid:high])
         merged = merge(left, right)
         return merged
 
 def quicksort_hybrid(lst: list) -> None:
-    """ Quicksort and insertionsort hybrid. Ones quicksoert have divided the data in to smaller lists of less then 10 the insertionsort will sort it because that is a lot faster. """
+    """ Quicksort and insertionsort hybrid."""
     low = 0
     high = len(lst)-1
     my_quicksort_hybrid(lst, low, high)
-    
+
 def my_quicksort_hybrid(lst, low, high):
     while low<high:
-
-        if high-low + 1 < 10:
+        if high-low + 1 < 43:
             my_insertionsort(lst, low, high)
             break
- 
         else:
             pivot = partition(lst, low, high)
-
             if pivot-low<high-pivot:
                 my_quicksort_hybrid(lst, low, pivot-1)
                 low = pivot + 1
@@ -137,17 +135,29 @@ def my_quicksort_hybrid(lst, low, high):
     return lst
 
 def main():
-    """ I have used main in other to test runtime but in this instance this script functions is getting called from tests.py which means that this will never get used. """
+    """instead of tests.py. will never get used. """
     avg = 0
-    for i in range(0,5):
-        input_list = np.random.randint(1000, 999999, size=100000).tolist()
-        timestamp_before = time.perf_counter()
-        mergesort_hybrid(input_list)
-        timestamp_after = time.perf_counter()
-        runtime = timestamp_after - timestamp_before
-        print(runtime)
-        avg = avg + runtime
-    print(f"avg {avg/5}")
+    avgavg = 0
+    number = 10
+    print(number)
+    for abc in range(0,5):
+        for bcd in range(0,5):
+            input_list = []
+            input_list = np.random.randint(1000, 999999, size=number).tolist()
+            timestamp_before = time.perf_counter()
+            mergesort_hybrid(input_list)
+            timestamp_after = time.perf_counter()
+            runtime = timestamp_after - timestamp_before
+            avg = avg + runtime
+        avg = avg / 5
+        avgavg = avgavg + avg
+        print(f"avg {avg}")
+        avg = 0
+    avgavg = avgavg / 5
+    print(f"avgavg {avgavg}")
+    constant = avgavg / number * math.log(number)
+    # c = avgavg / n**2
+    print(f"C {constant}")
 
 if __name__ == "__main__":
     main()
